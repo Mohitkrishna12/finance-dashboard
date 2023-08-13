01129c12c0c9e839eb22a3bfc556e4cec6ac107a
Mohit Krishna <mohitkrishna.s.s12@gmail.com>
2023-08-13 18:17:12 +0530

    add product & customer routes with models

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,32 @@
-import express from 'express';
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
 
-const router = express.Router();
+export const getProducts = async(req,res)=>{
+    try {
+        const products = await Product.find();
+        const productsWithStat = await Promise.all(
+          products.map(async (product) => {
+            const stat = await ProductStat.find({
+              productId: product._id,
+            });
+            return {
+              ...product._doc,
+              stat,
+            };
+          })
+        );
+        res.status(200).json(productsWithStat)
+    } catch (error) {
+         res.status(400).json({ message: error.message });
+    }
+}
 
-export default router;
+export const getCustomers = async(req,res)=>{
+    try {
+        const customers = await User.find({role:"user"}).select("-password");
+        res.status(200).json(customers)
+    } catch (error) {
+         res.status(400).json({ message: error.message });
+    }
+}
