736fa8ba115b0bf85a60375503dcc5f4cbbb7f62
Mohit Krishna <mohitkrishna.s.s12@gmail.com>
2023-08-15 17:08:49 +0530

    complete dashboard route and controller

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {getUser} from "../controllers/general.js"
+import {getUser,getDashboardStats} from "../controllers/general.js"
 
 const router = express.Router();
-
+router.get("/dashboard",getDashboardStats);
 router.get("/user/:id",getUser);
+
 
 export default router;
